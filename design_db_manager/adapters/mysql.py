cd9d56582ddd37d0a54ae4f8f897d80a44a32993
kkiyama117 <k.kiyama117@gmail.com>
2019-06-12 22:39:58 +0900

    Update function

--- design_db_manager/adapters/mysql.py
+++ design_db_manager/adapters/mysql.py
@@ -1,29 +1,49 @@
+from itertools import chain
+from typing import Optional, List
+
 import pymysql.cursors
 
 
-def connector(db: str, host='127.0.0.1', user='root', port=3306, **kwargs):
-    return pymysql.connect(host=host,
-                           port=port,
-                           user=user,
-                           db=db,
-                           charset='utf8mb4',
-                           cursorclass=pymysql.cursors.DictCursor,
-                           **kwargs
-                           )
+class MySQLManager:
+    def __init__(self, db: str, host: str = '127.0.0.1', port: int = 3306, user: str = 'root',
+                 password: Optional[str] = None, charset: str = 'utf8mb4'):
+        self.db = db
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.charset = charset
 
+    def _connector(self, **kwargs):
+        return pymysql.connect(host=self.host,
+                               port=self.port,
+                               user=self.user,
+                               password=self.password,
+                               db=self.db,
+                               charset=self.charset,
+                               # cursorclass=pymysql.cursors.DictCursor,
+                               **kwargs
+                               )
 
-def get_desc(user, database, **kwargs):
-    # Query to get employees who joined in a period defined by two dates
-    query = "show tables"
-    conn = connector(db=database, user=user, **kwargs)
-    try:
-        with conn as cursor:
-            cursor.execute(query)
-            result = cursor.fetchall()
-            print(result)
-    finally:
-        conn.close()
+    def get_tables(self) -> iter:
+        """
+
+        Returns:
+            iter: iterable of table names
+
+        """
+        # Query to get employees who joined in a period defined by two dates
+        query = "show tables"
+        conn = self._connector()
+        try:
+            with conn as cursor:
+                cursor.execute(query)
+                tables_data = cursor.fetchall()
+                return chain.from_iterable(tables_data)
+        finally:
+            conn.close()
 
 
 if __name__ == '__main__':
-    get_desc("kiyama", "test_db", password="19980117")
+    print(list(MySQLManager("test_db", user="kiyama", password="19980117").get_tables()))
+    # print(MySQLManager("test_db", user="kiyama", password="19980117").get_tables())
